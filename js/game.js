66ba05be27a75f9a438c9eff00686c6cfc118e91
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-10 19:23:36 +0200

    player functionality controls and meteor movements

--- js/game.js
+++ js/game.js
@@ -1,8 +1,7 @@
-//GAME CONTROLS AND OTHER ACTIONS 
-
+//GAME CONTROLS AND OTHER ACTIONS
 
 class Game {
-constructor(create,draw){
+  constructor(create, draw) {
     this.player = null;
     this.draw = draw;
     this.spaceArr = [];
@@ -10,35 +9,32 @@ constructor(create,draw){
     this.pointCounter = 0;
     this.timer = 0;
     this.meteorCounter = 0;
-}
+  }
 
-start(){
+  start() {
+    this.player = new Player();
+    this.player.domEl = this.create("player");
+    this.draw(this.player);
+  }
 
-this.player = new Player();
-this.player.domEl = this.create("player");
-this.draw(this.player);
-}
-
-movingPlayer(direction){
-    if(direction === "up" && this.player.heightPos < 90){
-        this.player.moveUp();
-        console.log('player is moving up');
-    }else if( direction === "down" && this.player.heightPos > 0){
-        this.player.moveDown();
-        console.log('player is moving down');
-    }else if( direction === "right" && this.player.widthPos < 90){
-        this.player.moveRight();
-        console.log("player is moving right");
-    }else if( direction === "left" && this.player.widthPos > 0){
-        this.player.moveLeft();
-        console.log('player is moving left');
-            }
+  movingPlayer(direction) {
+    if (direction === "up" && this.player.heightPos < 90) {
+      this.player.moveUp();
+      console.log("player is moving up");
+    } else if (direction === "down" && this.player.heightPos > 0) {
+      this.player.moveDown();
+      console.log("player is moving down");
+    } else if (direction === "right" && this.player.widthPos < 90) {
+      this.player.moveRight();
+      console.log("player is moving right");
+    } else if (direction === "left" && this.player.widthPos > 0) {
+      this.player.moveLeft();
+      console.log("player is moving left");
+    }
 
     this.draw(this.player);
-
-    }
+  }
 }
-
 
 class Player {
   constructor() {
@@ -72,24 +68,21 @@ class Player {
       this.widthPos += 5;
     }
   }
-
 }
-
 
 class Meteors {
-constructor(){
+  constructor() {
     this.widthPos = Math.floor(Math.random() * 90);
-    this.heightPos = Math.floor(Math.random()* 90);
+    this.heightPos = Math.floor(Math.random() * 90);
     this.height = Math.floor(5 + Math.random() * 10);
     this.width = Math.floor(5 + Math.random() * 10);
+  }
 
-}
-
-moveLeft(){
+  moveLeft() {
     this.widthPos--;
-}
+  }
 
-moveDown(){
+  moveDown() {
     this.heightPos--;
-}
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,46 +1,42 @@
 //GAME LOGIC HERE!!!!!!!!!!!!
 
-function createDomEl(className){
-    const board = document.getElementById("board");
-    const newEl = document.createElement("div");
-    newEl.className = className;
+function createDomEl(className) {
+  const board = document.getElementById("board");
+  const newEl = document.createElement("div");
+  newEl.className = className;
 
-    board.appendChild(newEl);
+  board.appendChild(newEl);
 
-    return newEl; 
+  return newEl;
 }
 
-function drawElements (instance){
+function drawElements(instance) {
+  instance.domEl.style.side = instance.heightPos + "%";
+  instance.domEl.style.upDown = instance.widthPos + "%";
 
-    instance.domEl.style.side = instance.heightPos + "%";
-    instance.domEl.style.upDown = instance.widthPos + "%";
-
-    instance.domEl.style.width = instance.width + "%";
-    instance.domEl.style.height = instance.height + "%";
+  instance.domEl.style.width = instance.width + "%";
+  instance.domEl.style.height = instance.height + "%";
 }
 
- const game = new Game (createDomEl, drawElements);
- game.start();
+const game = new Game(createDomEl, drawElements);
+game.start();
 
+document.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "ArrowRight":
+      game.movePlayer("right");
+      break;
+    case "ArrowLeft":
+      game.movePlayer("left");
 
+      break;
+    case "ArrowUp":
+      game.movePlayer("up");
+      console.log("pressed up arrows");
+      break;
 
- document.addEventListener("keydown", function(event){
-
-    switch(event.key){
-        case "ArrowRight":
-            game.movePlayer("right")
-        break;
-        case "ArrowLeft":
-            game.movePlayer("left");
-           
-        break;
-        case "ArrowUp":
-            game.movePlayer("up");
-            console.log('pressed up arrows')
-            break;
-
-        case "ArrowDown":
-            game.movePlayer("down");
-            break;
-    }
+    case "ArrowDown":
+      game.movePlayer("down");
+      break;
+  }
 });
