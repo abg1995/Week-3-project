2f00a0fd05662da6c0357af9d4a76867deb4e76a
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-11 15:40:43 +0200

    abandon proyect

--- js/game.js
+++ js/game.js
@@ -11,16 +11,37 @@ class Game {
     this.meteorCounter = 0;
   }
 
-  start() {  //create and draw a player , still dont see a player tho....
+  start() {
+    //create and draw a player , still dont see a player tho....
     this.player = new Player();
     this.player.domEl = this.create("player");
     this.draw(this.player);
 
+    // meteors creation
 
+    let intervalId = setInterval(
+      () => {
+        if (this.meteorCounter === 30) {
+          this.meteor = new Meteor();
+          this.meteor.domEl = this.create("meteor");
+          this.draw(this.meteor);
+          this.meteorArr.push(this.meteor);
+          this.meteorCounter = Math.floor(Math.random() * 30);
+        }
+        this.meteorArr.forEach((element) => {
+          element.moveDown();
+          element.moveLeft();
+          this.draw(element);
+          // this.detectCollision(element);
+        });
+        this.meteorCounter++;
+      },
+
+      80
+    );
   }
 
   movePlayer(direction) {
-
     if (direction === "up" && this.player.heightPos < 90) {
       this.player.moveUp();
       console.log("player is moving up");
@@ -37,11 +58,17 @@ class Game {
     this.draw(this.player);
   }
 
-
-  gameOver() {
-    alert("game over :(");
-    location.reload();
+  deleteMeteors(meteor) {
+    if (meteor.heightPos === 0) {
+      this.meteorArr.splice(this.meteorArr.indexOf(obstacle), 1);
+      meteor.domEl.remove();
+    }
   }
+
+  // gameOver() {
+  //   alert("game over :(");
+  //   location.reload();
+  // }
 }
 
 class Player {
@@ -55,35 +82,35 @@ class Player {
 
   moveUp() {
     if (this.heightPos < 90) {
-      this.heightPos += 2.5;
+      this.heightPos += 4;
     }
   }
 
   moveDown() {
     if (this.heightPos > 0) {
-      this.heightPos -= 2.5;
+      this.heightPos -= 4;
     }
   }
 
   moveLeft() {
     if (this.widthPos > 0) {
-      this.widthPos -= 2;
+      this.widthPos -= 4;
     }
   }
 
   moveRight() {
     if (this.width < 92) {
-      this.widthPos += 2;
+      this.widthPos += 4;
     }
   }
 }
 
-class Meteors {
+class Meteor {
   constructor() {
-    this.widthPos = Math.floor(Math.random() * 90);
-    this.heightPos = Math.floor(Math.random() * 90);
-    this.height = Math.floor(5 + Math.random() * 10);
-    this.width = Math.floor(5 + Math.random() * 10);
+    this.widthPos = Math.floor(Math.random() * 95);
+    this.heightPos = 95;
+    this.height = 5;
+    this.width = 5;
     this.domEl = null;
   }
 
@@ -94,4 +121,8 @@ class Meteors {
   moveDown() {
     this.heightPos--;
   }
+}
+
+class Bonus {
+  
 }
